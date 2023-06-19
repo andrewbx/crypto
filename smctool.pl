8edc5a0378fa599a17ea19a758faa329927cc1c4
Andrew <andrew@devnull.uk>
2023-06-19 14:52:02 +0100

    Add API query support for GoPlus and CoinGecko.

--- smctool.pl
+++ smctool.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 #--------------------------------------------------------------------------
 # Program     : smctool.pl
-# Version     : v1.0-STABLE-2023-06-04
+# Version     : v1.1-STABLE-2023-06-19
 # Description : Check Blockchain Smart Contract Health
 # Syntax      : smctool.pl <option>
 # Author      : Andrew (andrew@devnull.uk)
@@ -25,9 +25,10 @@ $Data::Dumper::Sortkeys  = 0;
 
 binmode( STDOUT, ":encoding(UTF-8)" );
 
-my $VERSION = "v1.0-STABLE";
+my $VERSION = "v1.1-STABLE";
 my $RELEASE = "smcTOOL $VERSION";
-my $API_URL = "https://api.gopluslabs.io/api/v1";
+my $GPL_URL = "https://api.gopluslabs.io/api/v1";
+my $CGO_URL = "https://api.coingecko.com/api/v3";
 
 @ARGV or help();
 
@@ -40,6 +41,8 @@ my $API_URL = "https://api.gopluslabs.io/api/v1";
         'o|output=s'        => \$opts{output},
         'c|cid=i'           => \$opts{cid},
         'a|address=s'       => \$opts{address},
+        'i|api=s'           => \$opts{api},
+        'query_api=s'       => \$args{query_api},
         'token_security'    => \$args{token_security},
         'approval_security' => \$args{approval_security},
         'rugpull_detect'    => \$args{rugpull_detect},
@@ -57,24 +60,39 @@ my $API_URL = "https://api.gopluslabs.io/api/v1";
         exit print("$RELEASE\n");
     }
 
+    if ( $opts{api} and $opts{api} !~ m/^(cgo|gpl)$/i ) {
+        print "API endpoint invalid\n";
+        exit help();
+    }
+
+    if ( $opts{api} and $opts{api} =~ m/^(cgo|gpl)$/i and !$args{query_api} ) {
+        print "API request expected\n";
+        exit help();
+    }
+
+    if ( $args{query_api} and $opts{api} ) {
+        my $API_URL = lc($opts{api}) eq q{gpl} ? $GPL_URL : $CGO_URL;
+        process_api( \%opts, { api => $API_URL, request => $args{query_api} } );
+    }
+
     if ( $args{token_security} ) {
-        process_api( \%opts, { request => q{token_security} } );
+        process_api( \%opts, { api => $GPL_URL, request => q{token_security} } );
     }
 
     if ( $args{approval_security} ) {
-        process_api( \%opts, { request => q{approval_security} } );
+        process_api( \%opts, { api => $GPL_URL, request => q{approval_security} } );
     }
 
     if ( $args{rugpull_detect} ) {
-        process_api( \%opts, { request => q{rugpull_detecting} } );
+        process_api( \%opts, { api => $GPL_URL, request => q{rugpull_detecting} } );
     }
 
     if ( $args{nft_security} ) {
-        process_api( \%opts, { request => q{nft_security} } );
+        process_api( \%opts, { api => $GPL_URL, request => q{nft_security} } );
     }
 
     if ( $args{address_security} ) {
-        process_api( \%opts, { request => q{address_security} } );
+        process_api( \%opts, { api => $GPL_URL, request => q{address_security} } );
     }
 }
 
@@ -86,22 +104,25 @@ sub help {
 \033[1m$RELEASE\033[0m - Retrieve blockchain smart contract information.
 
 \033[1mUsage:\033[0m
-  --token_security	-c|--cid -a|--address	<List smart contract attributes>
-  --approval_security	-c|--cid -a|--address	<List attributes to identify malicious behaviour>
-  --rugpull_detect	-c|--cid -a|--address	<List attributes to identify rug pull behaviour>
-  --nft_security	-c|--cid -a|--address	<List attributes configured for NFT's>
-  --address_security	-c|--cid -a|--address	<Check for malicious address>
+  --token_security     <-c|--cid -a|--address>  List smart contract attributes
+  --approval_security  <-c|--cid -a|--address>  List attributes to identify malicious behaviour
+  --rugpull_detect     <-c|--cid -a|--address>  List attributes to identify rug pull behaviour
+  --nft_security       <-c|--cid -a|--address>  List attributes configured for NFT's
+  --address_security   <-c|--cid -a|--address>  Check for malicious address
+  --query_api          <request>                Run an API query against an endpoint
 
 \033[1mOptions:\033[0m
-  -o|output	<json|dumper>		Output format for API Query. (Default=json)
-  -c|cid	<chain id>		Blockchain ID.
-  -a|address	<blockhain address>	Smart Contract or Holder address.
-  --help				Print this help information.
-  --version				Print version.
+  -o|output   <json|dumper>         Output format for API Query. (Default=json)
+  -c|cid      <chain id>            Blockchain ID.
+  -a|address  <blockhain address>   Smart Contract or Holder address.
+  -i|api      <cgo|gpl>             API Endpoint (gpl=GoPlusLabs, cgo=Coin Gecko)
+  --help                            Print this help information.
+  --version                         Print version.
 
 \033[1mReferences:\033[0m
 
-API Documentation - https://docs.gopluslabs.io/reference/
+GoPlusLabs API Documentation - https://docs.gopluslabs.io/reference/
+Coin Gecko API Documentation - https://www.coingecko.com/en/api/documentation
 
 " );
 
@@ -113,13 +134,21 @@ API Documentation - https://docs.gopluslabs.io/reference/
 sub process_api {
     my ( $opts, $argv ) = @_;
 
+    if ( $opts->{api} ) {
+        my $env = query_api( $argv->{api}, $argv->{request} );
+        if ( length($env) > 0 ) {
+            output_api( $opts->{output}, $env );
+        }
+        return;
+    }
+
     if ( !$opts->{cid} or !$opts->{address} ) {
         print "Blockchain ID and Smart Contract address expected\n";
         exit help();
     }
 
     $opts->{address} = lc( $opts->{address} );
-    my $env = query_api( $API_URL,
+    my $env = query_api( $argv->{api},
         qq{$argv->{request}/$opts->{cid}?contract_addresses=$opts->{address}} );
 
     if ( $env->{result} ) {
@@ -129,7 +158,7 @@ sub process_api {
     }
     else {
         print "\nNo results found.\n";
-        exit;
+        return;
     }
 }
 
@@ -140,7 +169,7 @@ sub query_api {
 
     my $ua = LWP::UserAgent->new(
         ssl_opts      => { verify_hostname => 0, SSL_verify_mode => 0x00 },
-        show_progress => 0
+        show_progress => 1
     );
 
     my $res = $ua->get( "$url/$argv", );
